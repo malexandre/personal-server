648bbc079b58b2e36b51a1bf737425f9e81af9aa
Marc Alexandre <github@alexandre.fr>
2019-07-28 14:55:46 +0200

    Fix and clean up nginx conf

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 git pull --rebase
-mkdir -p staticfiles
-wget -O ./staticfiles/mbp15.html https://raw.githubusercontent.com/malexandre/mbp-1.5-pool-generator/master/index.html
+mkdir -p www
+mkdir -p www/staticfiles
+wget -O www/staticfiles/mbp15.html https://raw.githubusercontent.com/malexandre/mbp-1.5-pool-generator/master/index.html
 docker-compose up -d
